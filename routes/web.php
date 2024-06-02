e2a54c7eb8a67b1ad9922983b4206296e53c35a8
MirzaMlna <mirzamaulana713@gmail.com>
2024-06-02 19:38:00 +0800

    Video 7.Model (WPU)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Posts\Post;
+
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -14,49 +16,14 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts/posts_view', [
         "title" => "Blog",
-        'posts' =>
-        [
-            [
-                'id' => 1,
-                'slug' => 'article-1',
-                'title' => 'Article 1',
-                'author' => 'Muhammad Mirza Maulana',
-                'body' => 'Matahari terbenam di balik pegunungan, mewarnai langit dengan gradasi oranye dan merah yang memukau. Suara burung-burung yang kembali ke sarang menciptakan harmoni alami yang menenangkan. Di kejauhan, siluet pepohonan terlihat seperti lukisan di atas kanvas alam. Hembusan angin sore yang sejuk membawa aroma tanah basah, hasil dari hujan ringan yang baru saja reda. Momen seperti ini mengingatkan kita akan keindahan dan ketenangan yang bisa ditemukan dalam alam, jauh dari hiruk-pikuk kehidupan kota.',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'article-2',
-                'title' => 'Article 2',
-                'author' => 'Muhammad Mirza Maulana',
-                'body' => 'Di sebuah desa kecil di tepi sungai, kehidupan bergerak dengan ritme yang berbeda. Anak-anak bermain di lapangan dengan tawa riang, sementara para petani kembali dari sawah dengan wajah penuh kepuasan setelah hari kerja yang panjang. Nenek-nenek duduk di beranda rumah, bercerita tentang masa lalu yang penuh kenangan. Sungai yang mengalir tenang menjadi saksi bisu dari kisah-kisah sederhana namun sarat makna yang terjadi di desa itu. Kehidupan di sana, meski tampak sederhana, menyimpan kedamaian yang sering kali sulit ditemukan di tempat lain.',
-            ]
-        ]
+        'posts' => Post::all(),
     ]);
 });
 
 
 Route::get('/post/{id}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'article-1',
-            'title' => 'Article 1',
-            'author' => 'Muhammad Mirza Maulana',
-            'body' => 'Matahari terbenam di balik pegunungan, mewarnai langit dengan gradasi oranye dan merah yang memukau. Suara burung-burung yang kembali ke sarang menciptakan harmoni alami yang menenangkan. Di kejauhan, siluet pepohonan terlihat seperti lukisan di atas kanvas alam. Hembusan angin sore yang sejuk membawa aroma tanah basah, hasil dari hujan ringan yang baru saja reda. Momen seperti ini mengingatkan kita akan keindahan dan ketenangan yang bisa ditemukan dalam alam, jauh dari hiruk-pikuk kehidupan kota.',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'article-2',
-            'title' => 'Article 2',
-            'author' => 'Muhammad Mirza Maulana',
-            'body' => 'Di sebuah desa kecil di tepi sungai, kehidupan bergerak dengan ritme yang berbeda. Anak-anak bermain di lapangan dengan tawa riang, sementara para petani kembali dari sawah dengan wajah penuh kepuasan setelah hari kerja yang panjang. Nenek-nenek duduk di beranda rumah, bercerita tentang masa lalu yang penuh kenangan. Sungai yang mengalir tenang menjadi saksi bisu dari kisah-kisah sederhana namun sarat makna yang terjadi di desa itu. Kehidupan di sana, meski tampak sederhana, menyimpan kedamaian yang sering kali sulit ditemukan di tempat lain.',
-        ]
-    ];
 
-    $post =
-        Arr::first($posts, function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        });
+    $post = Post::find($slug);
     return view(
         'posts/post_view',
         [
